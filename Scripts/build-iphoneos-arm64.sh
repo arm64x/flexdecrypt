17340b58816e47c543a0aeeb499fc86b5725a467
Lê Tí <106995604+arm64x@users.noreply.github.com>
2025-02-15 14:04:09 +0700

    Update build-iphoneos-arm64.sh

--- Scripts/build-iphoneos-arm64.sh
+++ Scripts/build-iphoneos-arm64.sh
@@ -7,7 +7,7 @@ scriptsDirectory=`dirname "$0"`
 projectDirectory="$scriptsDirectory/../"
 
 pushd "$projectDirectory"
-    sed -i '' 's/IPHONEOS_DEPLOYMENT_TARGET = 11.0;/IPHONEOS_DEPLOYMENT_TARGET = 15.0;/g' flexdecrypt.xcodeproj/project.pbxproj
+    sed -i '' 's/IPHONEOS_DEPLOYMENT_TARGET = 11.0;/IPHONEOS_DEPLOYMENT_TARGET = 16.0;/g' flexdecrypt.xcodeproj/project.pbxproj
     xcodebuild -scheme flexdecrypt -sdk iphoneos -configuration Release -derivedDataPath ./derived-rootless
     binDirectory="./Package-rootless/var/jb/usr/bin"
     mkdir -p "$binDirectory"
